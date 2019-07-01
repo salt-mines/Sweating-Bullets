d53b98957696966b0168290a65589378e540233d
Aleksi Kervinen <me@aleksi.me>
2019-07-01 13:25:48 +0300

    improve player input slightly

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float mouseSensitivity = 0.1f;
+
+    public float Forward { get; private set; }
+    public float Strafe { get; private set; }
+
+    public float MouseX { get; private set; }
+    public float MouseY { get; private set; }
+
+    void Update()
+    {
+        Forward = Input.GetAxisRaw("Vertical");
+        Strafe = Input.GetAxisRaw("Horizontal");
+
+        MouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        MouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+    }
+}
