3cddcdf4493b707ece782594775a03a2a38996fb
Aleksi Kervinen <me@aleksi.me>
2019-07-13 23:09:45 +0300

    reorganize assets, create loading screen

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    public Canvas loadingScreenCanvas;
+    public SceneReference mainMenuScene;
+    public ProgressBar progressBar;
+
+    private void Start()
+    {
+        if (SceneManager.sceneCount == 1)
+            // If this is the only loaded scene, load main menu
+            LoadScene(mainMenuScene.ScenePath);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void LoadScene(string name)
+    {
+        if (loadingScreenCanvas)
+            loadingScreenCanvas.gameObject.SetActive(true);
+        
+        StartCoroutine(LoadSceneAsync(name));
+    }
+
+    private IEnumerator LoadSceneAsync(string name)
+    {
+        var op = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+        op.allowSceneActivation = false;
+
+        while (op.progress < 0.9f)
+        {
+            if (progressBar)
+                progressBar.Progress = op.progress;
+
+            yield return null;
+        }
+
+        op.allowSceneActivation = true;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (loadingScreenCanvas)
+            loadingScreenCanvas.gameObject.SetActive(false);
+        
+        SceneManager.SetActiveScene(scene);
+    }
+}
---
+++ Assets/UI/Scripts/ProgressBar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(0, 1)]
+        private float progress;
+
+        public RectTransform progressBar;
+
+        public float Progress
+        {
+            get => progress;
+            set => SetProgress(value);
+        }
+
+        // Start is called before the first frame update
+        private void OnValidate()
+        {
+            SetProgress(progress);
+        }
+
+        private void SetProgress(float newProgress)
+        {
+            progress = newProgress;
+
+            var newScale = progressBar.localScale;
+            newScale.x = progress;
+            progressBar.localScale = newScale;
+        }
+    }
+}
