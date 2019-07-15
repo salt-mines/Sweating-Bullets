0399e3b0dcfa8020f2f3131598bfd6b064b6b58c
Aleksi Kervinen <me@aleksi.me>
2019-07-15 20:05:14 +0300

    improve jump pad

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -4,9 +4,23 @@ public class JumpPad : MonoBehaviour
 {
     public float jumpStrength = 20f;
 
+    public bool keepPlayerVelocity;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-            other.gameObject.GetComponent<PlayerMovement>().Velocity = transform.up * jumpStrength;
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        var pm = other.gameObject.GetComponent<PlayerMovement>();
+        if (!pm) return;
+
+        var vel = transform.up * jumpStrength;
+
+        if (keepPlayerVelocity)
+        {
+            vel.x += pm.Velocity.x;
+            vel.z += pm.Velocity.z;
+        }
+        
+        pm.Velocity = vel;
     }
 }
